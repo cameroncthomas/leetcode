2eac55974a525486b43f10b693dcc3e23a5b0f87
cameroncthomas <167522464+cameroncthomas@users.noreply.github.com>
2025-10-12 14:54:38 +0100

    feat(python): update explanation for 0011

--- python/0011-container-with-most-water.py
+++ python/0011-container-with-most-water.py
@@ -3,14 +3,15 @@ class Solution:
         # Idea:
         # - Brute force approach
         # Calculate all areas whilst keeping track of max_area. Return max_area.
-        # TC: O(n^2), SC: O(1)
+        # TC: O(n^2) where n is length of height array, SC: O(1).
         # - Optimal approach
         # Greedy algo with two pointers.
         # Initialise pointers at either end of height array. Then update pointer
         # corresponding to smaller height. This opens up possibility of achieving
         # a larger current area and so is locally optimal.
         # Keep track of max_area and then return max_area.
-        # TC: one-pass O(n), SC: O(1)
+        # TC: one-pass O(n), SC: O(1).
+
         max_area = 0
         left, right = 0, len(height) - 1
 
